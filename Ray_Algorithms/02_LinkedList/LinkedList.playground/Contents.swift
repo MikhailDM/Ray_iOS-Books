76e28899de62e5eb29459bfb71d79c2b041dc626
Mikhail Dmitriev <misha.dmitriev.spb@gmail.com>
2021-01-12 11:51:19 +0300

    Stacks. Refactor

--- Ray_Algorithms/02_LinkedList/LinkedList.playground/Contents.swift
+++ Ray_Algorithms/02_LinkedList/LinkedList.playground/Contents.swift
@@ -1,15 +1,8 @@
-// Copyright (c) 2019 Razeware LLC
-// For full license & permission details, see LICENSE.markdown.
-
 import Foundation
 
 public struct Stack<Element> {
     
     private var storage: [Element] = []
-    
-//    public init(_ elements: [Element]) {
-//        storage = elements
-//    }
     
     public mutating func push(_ element: Element) {
         storage.append(element)
@@ -45,26 +38,6 @@ extension Stack: CustomStringConvertible {
         """
     }
 }//
-
-//example(of: "using a stack") {
-//    var stack = Stack<Int>()
-//    stack.push(1)
-//    stack.push(2)
-//    stack.push(3)
-//    stack.push(4)
-//    print(stack)
-//    if let poppedElement = stack.pop() {
-//        assert(4 == poppedElement)
-//        print("Popped: \(poppedElement)")
-//    }
-//}
-
-//example(of: "initializing a stack from an array") {
-//    let array = ["A", "B", "C", "D"]
-//    var stack = Stack(array)
-//    print(stack)
-//    stack.pop()
-//}
 
 example(of: "initializing a stack from an array literal") {
     var stack: Stack = [1.0, 2.0, 3.0, 4.0]
