2aa2f03ce160e0254e6ee8fa99aebd7e37fd1829
Mikhail Dmitriev <misha.dmitriev.spb@gmail.com>
2021-01-12 13:11:48 +0300

    Linked List

--- Ray_Algorithms/02_LinkedList/LinkedList.playground/Contents.swift
+++ Ray_Algorithms/02_LinkedList/LinkedList.playground/Contents.swift
@@ -1,88 +1,140 @@
 import Foundation
 
-public struct Stack<Element> {
-    
-    private var storage: [Element] = []
-    
-    public mutating func push(_ element: Element) {
-        storage.append(element)
-    }
-    
-    @discardableResult
-    public mutating func pop() -> Element? {
-        storage.popLast()
-    }
-    
-    public func peek() -> Element? {
-        storage.last
-    }
-    
-    public var isEmpty: Bool {
-        peek() == nil
-    }
-    
-}//
-
-extension Stack: ExpressibleByArrayLiteral {
-    public init(arrayLiteral elements: Element...) {
-        storage = elements
-    }
-}//
-
-extension Stack: CustomStringConvertible {
-    public var description: String {
-        """
-        ----top----
-        \(storage.map { "\($0)" }.reversed().joined(separator: "\n"))
-        -----------
-        """
-    }
-}//
-
-example(of: "initializing a stack from an array literal") {
-    var stack: Stack = [1.0, 2.0, 3.0, 4.0]
-    print(stack)
-    stack.pop()
-}//
-
-//MARK: - Challenge 1. Reverse an Array
-//Create a function that prints the contents of an array in reversed order
-func printInReverse<T>(_ array: [T]) {
-    var stack = Stack<T>()
-    for value in array {
-        stack.push(value)
-    }
-    while let value = stack.pop() {
-        print(value)
-    }
-}//
-
-example(of: "Challenge 1") {
-    let stack: Stack = [1.0, 2.0, 3.0, 4.0]
-    print(stack)
-    printInReverse([1.0, 2.0, 3.0, 4.0])
+example(of: "creating and linking nodes") {
+    let node1 = Node(value: 1)
+    let node2 = Node(value: 2)
+    let node3 = Node(value: 3)
+    node1.next = node2
+    node2.next = node3
+    print(node1)
 }
+
+example(of: "push") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    print(list)
+}
+
+example(of: "append") {
+    var list = LinkedList<Int>()
+    list.append(1)
+    list.append(2)
+    list.append(3)
+    print(list)
+}
+
+example(of: "inserting at a particular index") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    print("Before inserting: \(list)")
+    var middleNode = list.node(at: 1)!
+    for _ in 1...4 {
+        middleNode = list.insert(-1, after: middleNode)
+    }
+    print("After inserting: \(list)")
+}
+
+example(of: "pop") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    print("Before popping list: \(list)")
+    let poppedValue = list.pop()
+    print("After popping list: \(list)")
+    print("Popped value: " + String(describing: poppedValue))
+}
+
+example(of: "removing the last node") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    print("Before removing last node: \(list)")
+    let removedValue = list.removeLast()
+    print("After removing last node: \(list)")
+    print("Removed value: " + String(describing: removedValue))
+}
+
+example(of: "removing a node after a particular node") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    print("Before removing at particular index: \(list)")
+    let index = 1
+    let node = list.node(at: index - 1)!
+    let removedValue = list.remove(after: node)
+    print("After removing at index \(index): \(list)")
+    print("Removed value: " + String(describing: removedValue))
+}
+
+example(of: "using collection") {
+    var list = LinkedList<Int>()
+    for i in 0...9 {
+        list.append(i)
+    }
+    
+    print("List: \(list)")
+    print("First element: \(list[list.startIndex])")
+    print("Array containing first 3 elements: \(Array(list.prefix(3)))")
+    print("Array containing last 3 elements: \(Array(list.suffix(3)))")
+    let sum = list.reduce(0, +)
+    print("Sum of all values: \(sum)")
+}
+
+example(of: "array cow") {
+    let array1 = [1, 2]
+    var array2 = array1
+    print("array1: \(array1)")
+    print("array2: \(array2)")
+    print("---After adding 3 to array 2---")
+    array2.append(3)
+    print("array1: \(array1)")
+    print("array2: \(array2)")
+}
+
+example(of: "linked list cow") {
+    var list1 = LinkedList<Int>()
+    list1.append(1)
+    list1.append(2)
+    
+    print("List1 uniquely referenced: \(isKnownUniquelyReferenced(&list1.head))")
+    var list2 = list1
+    print("List1 uniquely referenced: \(isKnownUniquelyReferenced(&list1.head))")
+        
+    print("List1: \(list1)")
+    print("List2: \(list2)")
+    print("After appending 3 to list2")
+    list2.append(3)
+    print("List1: \(list1)")
+    print("List2: \(list2)")
+    print("Removing middle node on list2")
+    if let node = list2.node(at: 0) {
+      list2.remove(after: node)
+    }
+    print("List2: \(list2)")
+}
+
+example(of: "sharing nodes") {
+    var list1 = LinkedList<Int>()
+    (1...3).forEach { list1.append($0) }
+    var list2 = list1
+    print("List1: \(list1)")
+    print("List2: \(list2)")
+    list2.push(0)
+    print("List1: \(list1)")
+    print("List2: \(list2)")
+    list1.push(100)
+    print("List1: \(list1)")
+    print("List2: \(list2)")
+}
+    
+//MARK: - Challenge 1. Reverse an Array
+
 
 //MARK: - Challenge 2. Balance the parentheses
-//Check for balanced parentheses. Given a string, check if there are ( and ) characters,
-//and return true if the parentheses in the string are balanced. For example:
-func checkParentheses(_ string: String) -> Bool {
-    var stack = Stack<Character>()
-    for character in string {
-        if character == "(" {
-            stack.push(character)
-        } else if character == ")" {
-            if stack.isEmpty {
-                return false
-            } else {
-                stack.pop()
-            }
-        }
-    }
-    return stack.isEmpty
-}
-
-example(of: "Challenge 2") {
-    print(checkParentheses("h((e))llo(world)()"))
-    print(checkParentheses("(hello world"))
-}
